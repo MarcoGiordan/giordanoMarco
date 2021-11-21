b84d5a1b0d04d4d59f6f26544644bc9b5f0e5bb0
MarcoGiordan <91075499+MarcoGiordan@users.noreply.github.com>
2021-11-21 21:36:15 +0100

    Add files via upload

--- python2.py
+++ python2.py
@@ -1,3 +1,6 @@
-x = 30
-y = 2
-print(x + y)
+A = int(input("inserisci il valore di A:\n"))
+B = int(input("inserisci il valore di B:\n"))
+if(A>B):
+    print("il numero più grande è " + str(A))
+elif(A<B):
+    print("il numero più grande è " + str(B))
